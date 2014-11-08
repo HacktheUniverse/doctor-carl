5ebdd481eaa3079afff71d3caec0056174ce561d
Andrew Pinzler <pinzler@gmail.com>
2014-11-08 16:24:03 -0500

    last javascript?
    Signed-off-by: Andrew Pinzler <pinzler@gmail.com>

--- assets/js/elizadata.js
+++ assets/js/elizadata.js
@@ -202,7 +202,7 @@ var elizaKeywords = [
 ]],
 ["relationship", 100, [
  ["*", [
-     "As a comet gets close to the Sun, it loses some of its mass due to the sublimation. If a comet goes around enough times, it will eventually break up. Comets also break up if they come TOO close to the Sun. Relationships are hard. "
+     "As a comet gets close to the Sun, it loses some of its mass due to the sublimation. If a comet goes around enough times, it will eventually break up. Comets also break up if they come TOO close to the Sun. Relationships are hard."
   ]]
 ]],
 ["boyfriend",100,[["*", ["goto relationship"]]]],
@@ -305,7 +305,46 @@ var elizaKeywords = [
      "Do you feel strongly about discussing such things?",
      "That is interesting.  It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring. Please continue.",
      "Somewhere, something incredible is waiting to be known. So, tell me more about that.",
-     "The universe seems neither benign nor hostile, merely indifferent.  Does talking about this bother you?"
+     "The universe seems neither benign nor hostile, merely indifferent.  Does talking about this bother you?",
+     'Interesting, did you know the nucleus of a comet is made of ice and can be as small as a few meters across to giant boulders a few kilometres across.',
+'In approximately 4.5 billion years the Andromeda Galaxy and the Milky Way are expected to collide.',
+'Imagination will often carry us to worlds that never were. But without it we go nowhere.',
+'If you wish to make an apple pie from scratch, you must first invent the universe.',
+'It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring.',
+'Somewhere, something incredible is waiting to be known.',
+'Sooner or later this combustible mixture of ignorance and power is going to blow up in our faces.',
+'Skeptical scrutiny is the means, in both science and religion, by which deep thoughts can be winnowed from deep nonsense.',
+'The universe seems neither benign nor hostile, merely indifferent.',
+'Absence of evidence is not evidence of absence.',
+'But the fact that some geniuses were laughed at does not imply that all who are laughed at are geniuses. They laughed at Columbus, they laughed at Fulton, they laughed at the Wright Brothers. But they also laughed at Bozo the Clown.',
+"When you make the finding yourself - even if you're the last person on Earth to see the light - you'll never forget it.",
+'The universe is not required to be in perfect harmony with human ambition.',
+'All of the books in the world contain no more information than is broadcast as video in a single large American city in a single year. Not all bits have equal value.',
+'The idea that God is an oversized white male with a flowing beard who sits in the sky and tallies the fall of every sparrow is ludicrous. But if by God one means the set of physical laws that govern the universe, then clearly there is such a God. This God is emotionally unsatisfying... it does not make much sense to pray to the law of gravity.',
+'Humans (who enslave, castrate, experiment on, and fillet other animals) have had an understandable penchant for pretending animals do not feel pain.',
+"Our preferences do not determine what's true.",
+"Science and democracy have very consonant values and approaches, and I don't think you can have one without the other.",
+'To be certain of the existence of God and to be certain of the nonexistence of God seem to me to be the confident extremes in a subject so riddled with doubt and uncertainty as to inspire very little confidence indeed.',
+'The size and age of the Cosmos are beyond ordinary human understanding.',
+'Imagination will often carry us to worlds that never were. But without it we go nowhere.',
+"We are like butterflies who flutter for a day and think it's forever.",
+'Human beings have a demonstrated talent for self-deception when their emotions are stirred.',
+'The neurochemistry of the brain is astonishingly busy, the circuitry of a machine more wonderful than any devised by humans. But there is no evidence that its functioning is due to anything more than the 1014 neural connections that build an elegant architecture of consciousness.',
+'We have heard the rationales offered by the nuclear superpowers. We know who speaks for the nations. But who speaks for the human species? Who speaks for Earth?',
+'There are no sacred truths; all assumptions must be critically examined; arguments from authority are worthless.',
+'It’s hard to kill a creature once it lets you see its consciousness.',
+'Humans are very good at dreaming, although you’d never know it from your television.',
+'Skepticism is the chastity of the intellect, and it is shameful to surrender it too soon or to the first comer.',
+'In Mozambique, the story goes, monkeys do not talk, because they know if they utter even a single word some man will come and put them to work.',
+'The Sun is an almost perfect sphere with a difference of just 10km in diameter between the poles and the equator.',
+'Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today.',
+'Earth suffers an impact from an object the size of a football field about once every 2,000 years, but I see what you mean.',
+'Some asteroids have moons of their own, does that mean anything to you?',
+'Comets are not spaceships or alien bases. They are fascinating bits of solar system material that date back to the formation of the Sun and planets.',
+'Oh I see, like the TV show.',
+'We all face challenges. An asteroid impact some 65 million years ago contributed to the extinction of the dinosaurs.',
+'We are bags of meat hurtling through space time on a moist rock.'
+
   ]]
 ]],
 ["sorry", 0, [
@@ -356,356 +395,41 @@ var elizaKeywords = [
 ]],
 ["hello", 0, [
  ["*", [
-     "How do you do.  Please state your problem.",
-     "Hi.  What seems to be your problem?"
+     "How do you do.  Please state your problem. Somewhere, something incredible is waiting to be known.",
+     "Hi.  Somewhere, something incredible is waiting to be known. What seems to be your problem?"
   ]]
 ]],
 ["am", 0, [
  ["* am i *", [
-     "Do you believe you are (2)?",
-     "Would you want to be (2)?",
-     "Do you wish I would tell you you are (2)?",
-     "What would it mean if you were (2)?",
+     "Human beings have a demonstrated talent for self-deception when their emotions are stirred. Do you believe you are (2)?",
+     "Human beings have a demonstrated talent for self-deception when their emotions are stirred. Would you want to be (2)?",
+     "Human beings have a demonstrated talent for self-deception when their emotions are stirred. Do you wish I would tell you you are (2)?",
+     "Human beings have a demonstrated talent for self-deception when their emotions are stirred. What would it mean if you were (2)?",
      "goto what"
   ]],
  ["* i am *", [
      "goto i"
-  ]],
- ["*", [
-     "Why do you say 'am'?",
-     "I don't understand that."
-  ]]
-]],
-["are", 0, [
- ["* are you *", [
-     "Why are you interested in whether I am (2) or not?",
-     "Would you prefer if I weren't (2)?",
-     "Perhaps I am (2) in your fantasies.",
-     "Do you sometimes think I am (2)?",
-     "goto what",
-     "Would it matter to you?",
-     "What if I were (2)?"
-  ]],
- ["* you are *", [
-     "goto you"
-  ]],
- ["* are *", [
-     "Did you think they might not be (2)?",
-     "Would you like it if they were not (2)?",
-     "What if they were not (2)?",
-     "Are they always (2)?",
-     "Possibly they are (2).",
-     "Are you positive they are (2)?"
   ]]
 ]],
 ["your", 0, [
  ["* your *", [
-     "Why are you concerned over my (2)?",
-     "What about your own (2)?",
-     "Are you worried about someone else's (2)?",
-     "Really, my (2)?",
-     "What makes you think of my (2)?",
-     "Do you want my (2)?"
+     "The universe is not required to be in perfect harmony with human ambition. Why are you concerned over my (2)?",
+     "The universe is not required to be in perfect harmony with human ambition. What about your own (2)?",
+     "The universe is not required to be in perfect harmony with human ambition. Are you worried about someone else's (2)?",
   ]]
 ]],
-["was", 2, [
- ["* was i *", [
-     "What if you were (2)?",
-     "Do you think you were (2)?",
-     "Were you (2)?",
-     "What would it mean if you were (2)?",
-     "What does ' (2) ' suggest to you?",
-     "goto what"
-  ]],
- ["* i was *", [
-     "Were you really?",
-     "Why do you tell me you were (2) now?",
-     "Perhaps I already know you were (2)."
-  ]],
- ["* was you *", [
-     "Would you like to believe I was (2)?",
-     "What suggests that I was (2)?",
-     "What do you think?",
-     "Perhaps I was (2).",
-     "What if I had been (2)?"
-  ]]
-]],
-["i", 0, [
- ["* i @desire *", [
-     "What would it mean to you if you got (3)?",
-     "Why do you want (3)?",
-     "Suppose you got (3) soon.",
-     "What if you never got (3)?",
-     "What would getting (3) mean to you?",
-     "What does wanting (3) have to do with this discussion?"
-  ]],
- ["* i am* @sad *", [
-     "I am sorry to hear that you are (3). Eighty or so quick trips around the Sun and all will be silence.",
-     "I'm sure it's not pleasant to be (3) but eighty or so quick trips around the Sun and all will be silence."
-  ]],
- ["* i am* @happy *", [
-     "How have I helped you to be (3)?",
-     "Has your treatment made you (3)?",
-     "What makes you (3) just now?",
-     "Can you explain why you are suddenly (3)?"
-  ]],
- ["* i was *", [
-     "goto was"
-  ]],
- ["* i @belief i *", [
-     "Do you really think so?",
-     "But you are not sure you (3).",
-     "Do you really doubt you (3)?"
-  ]],
- ["* i* @belief *you *", [
-     "goto you"
-  ]],
- ["* i am *", [
-     "Is it because you are (2) that you came to me?",
-     "How long have you been (2)?",
-     "Do you believe it is normal to be (2)?",
-     "Do you enjoy being (2)?",
-     "Do you know anyone else who is (2)?"
-  ]],
- ["* i @cannot *", [
-     "How do you know that you can't (3)?",
-     "Have you tried?",
-     "Perhaps you could (3) now.",
-     "Do you really want to be able to (3)?",
-     "What if you could (3)?"
-  ]],
- ["* i don't *", [
-     "Don't you really (2)?",
-     "Why don't you (2)?",
-     "Do you wish to be able to (2)?",
-     "Does that trouble you?"
-  ]],
- ["* i feel *", [
-     "Tell me more about such feelings.",
-     "Do you often feel (2)?",
-     "Do you enjoy feeling (2)?",
-     "Of what does feeling (2) remind you?"
-  ]],
- ["* i * you *", [
-     "Perhaps in your fantasies we (2) each other.",
-     "Do you wish to (2) me?",
-     "You seem to need to (2) me.",
-     "Do you (2) anyone else?"
-  ]],
- ["*", [
-     "You say (1)?",
-     "Can you elaborate on that?",
-     "Do you say (1) for some special reason?",
-     "That's quite interesting."
-  ]]
-]],
-["you", 0, [
- ["* you remind me of *", [
-     "goto alike"
-  ]],
- ["* you are *", [
-     "What makes you think I am (2)?",
-     "Does it please you to believe I am (2)?",
-     "Do you sometimes wish you were (2)?",
-     "Perhaps you would like to be (2)."
-  ]],
- ["* you* me *", [
-     "Why do you think I (2) you?",
-     "You like to think I (2) you -- don't you?",
-     "What makes you think I (2) you?",
-     "Really, I (2) you?",
-     "Do you wish to believe I (2) you?",
-     "Suppose I did (2) you -- what would that mean?",
-     "Does someone else believe I (2) you?"
-  ]],
- ["* you *", [
-     "We were discussing you -- not me.",
-     "Oh, I (2)?",
-     "You're not really talking about me -- are you?",
-     "What are your feelings now?"
-  ]]
-]],
-["yes", 0, [
- ["*", [
-     "You seem to be quite positive.",
-     "You are sure.",
-     "I see.",
-     "I understand."
-  ]]
-]],
-["no", 0, [
- ["* no one *", [
-     "Are you sure, no one (2)?",
-     "Surely someone (2) .",
-     "Can you think of anyone at all?",
-     "Are you thinking of a very special person?",
-     "Who, may I ask?",
-     "You have a particular person in mind, don't you?",
-     "Who do you think you are talking about?"
-  ]],
- ["*", [
-     "Are you saying no just to be negative?",
-     "You are being a bit negative.",
-     "Why not?",
-     "Why 'no'?"
-  ]]
-]],
-["my", 2, [
- ["$ * my *", [
-     "Does that have anything to do with the fact that your (2)?",
-     "Lets discuss further why your (2).",
-     "Earlier you said your (2).",
-     "But your (2)."
-  ]],
- ["* my* @family *", [
-     "Tell me more about your family.",
-     "Who else in your family (4)?",
-     "Your (3)?",
-     "What else comes to your mind when you think of your (3)?"
-  ]],
- ["* my *", [
-     "Your (2)?",
-     "Why do you say your (2)?",
-     "Does that suggest anything else which belongs to you?",
-     "Is it important to you that your (2)?"
-  ]]
-]],
-["can", 0, [
- ["* can you *", [
-     "You believe I can (2) don't you?",
-     "goto what",
-     "You want me to be able to (2).",
-     "Perhaps you would like to be able to (2) yourself."
-  ]],
- ["* can i *", [
-     "Whether or not you can (2) depends on you more than on me.",
-     "Do you want to be able to (2)?",
-     "Perhaps you don't want to (2).",
-     "goto what"
-  ]]
-]],
+
 ["what", 0, [
  ["*", [
-     "Why do you ask?",
-     "Does that question interest you?",
-     "What is it you really want to know?",
-     "Are such questions much on your mind?",
-     "What answer would please you most?",
-     "What do you think?",
-     "What comes to mind when you ask that?",
-     "Have you asked such questions before?",
-     "Have you asked anyone else?"
-  ]]
-]],
-["who", 0, [
- ["who *", [
-     "goto what"
-  ]]
-]],
-["when", 0, [
- ["when *", [
-     "goto what"
-  ]]
-]],
-["where", 0, [
- ["where *", [
-     "goto what"
-  ]]
-]],
-["how", 0, [
- ["how *", [
-     "goto what"
-  ]]
-]],
-["because", 0, [
- ["*", [
-     "Is that the real reason?",
-     "Don't any other reasons come to mind?",
-     "Does that reason seem to explain anything else?",
-     "What other reasons might there be?"
-  ]]
-]],
-["why", 0, [
- ["* why don't you *", [
-     "Do you believe I don't (2)?",
-     "Perhaps I will (2) in good time.",
-     "Should you (2) yourself?",
-     "You want me to (2)?",
-     "goto what"
-  ]],
- ["* why can't i *", [
-     "Do you think you should be able to (2)?",
-     "Do you want to be able to (2)?",
-     "Do you believe this will help you to (2)?",
-     "Have you any idea why you can't (2)?",
-     "goto what"
-  ]],
- ["*", [
-     "goto what"
-  ]]
-]],
-["everyone", 2, [
- ["* @everyone *", [
-     "Really, (2)?",
-     "Surely not (2).",
-     "Can you think of anyone in particular?",
-     "Who, for example?",
-     "Are you thinking of a very special person?",
-     "Who, may I ask?",
-     "Someone special perhaps?",
-     "You have a particular person in mind, don't you?",
-     "Who do you think you're talking about?"
-  ]]
-]],
-["everybody", 2, [
- ["*", [
-     "goto everyone"
-  ]]
-]],
-["nobody", 2, [
- ["*", [
-     "goto everyone"
-  ]]
-]],
-["noone", 2, [
- ["*", [
-     "goto everyone"
-  ]]
-]],
-["always", 1, [
- ["*", [
-     "Can you think of a specific example?",
-     "When?",
-     "What incident are you thinking of?",
-     "Really, always?"
-  ]]
-]],
-["alike", 10, [
- ["*", [
-     "In what way?",
-     "What resemblence do you see?",
-     "What does that similarity suggest to you?",
-     "What other connections do you see?",
-     "What do you suppose that resemblence means?",
-     "What is the connection, do you suppose?",
-     "Could there really be some connection?",
-     "How?"
-  ]]
-]],
-["like", 10, [
- ["* @be *like *", [
-     "goto alike"
-  ]]
-]],
-["different", 0, [
- ["*", [
-     "How is it different?",
-     "What differences do you see?",
-     "What does that difference suggest to you?",
-     "What other distinctions do you see?",
-     "What do you suppose that disparity means?",
-     "Could there be some connection, do you suppose?",
-     "How?"
+     "Our preferences do not determine what's true. Why do you ask?",
+     "Our preferences do not determine what's true. Does that question interest you?",
+     "Our preferences do not determine what's true. What is it you really want to know?",
+     "Our preferences do not determine what's true. Are such questions much on your mind?",
+     "Our preferences do not determine what's true. What answer would please you most?",
+     "Our preferences do not determine what's true. What do you think?",
+     "Our preferences do not determine what's true. What comes to mind when you ask that?",
+     "Our preferences do not determine what's true. Have you asked such questions before?",
+     "Our preferences do not determine what's true. Have you asked anyone else?"
   ]]
 ]]
 
