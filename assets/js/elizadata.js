fefd8c4ad87eda708bafaafd94c7304a08dc6205
Andrew Pinzler <pinzler@gmail.com>
2014-11-08 16:13:01 -0500

    more data
    Signed-off-by: Andrew Pinzler <pinzler@gmail.com>

--- assets/js/elizadata.js
+++ assets/js/elizadata.js
@@ -245,23 +245,74 @@ var elizaKeywords = [
   ]]
 ]],
 ["tears",100,[["*", ["goto crying"]]]],
+["cancer", 100, [
+ ["*", [
+     "Cancer is a medium-size constellation with an area of 506 square degrees and its stars are rather faint, its brightest star Beta Cancri having an apparent magnitude of 3.5."
+  ]]
+]],
+["circle", 100, [
+ ["*", [
+     "Comet orbits are usually elliptical."
+  ]]
+]],
+["cicrles",100,[["*", ["goto circle"]]]],
+["who am i", 100, [
+ ["*", [
+     "Who are we? We find that we live on an insignificant planet of a humdrum star lost in a galaxy tucked away in some forgotten corner of a universe in which there are far more galaxies than people."
+  ]]
+]],
+["who i am",100,[["*", ["goto who am i"]]]],
+["sad", 100, [
+ ["*", [
+     "I am sorry to hear that you are sad. Eighty or so quick trips around the Sun and all will be silence.",
+     "I'm sure it's not pleasant to be sad but eighty or so quick trips around the Sun and all will be silence."
+  ]]
+]],
+["debug", 100, [
+ ["*", [
+     "Are you debugging your code or is it debugging you?"
+  ]]
+]],
+["my cobe",100,[["*", ["goto debug"]]]],
+["sleep", 100, [
+ ["*", [
+     "Everynight we die and every dawn we are reborn. Such a dangerous game."
+  ]]
+]],
+["insomnia",100,[["*", ["goto sleep"]]]],
+["failure", 100, [
+ ["*", [
+     "Failure is not only possible it is inevitable. Billions of species before you have been extinguished from this terrestrial womb. And billions more are soon to follow."
+  ]]
+]],
+["fat", 100, [
+ ["*", [
+     "Weight is the inevitable outcome of a universe held together with gravity."
+  ]]
+]],
+["overweight",100,[["*", ["goto fat"]]]],
+["jerk", 100, [
+ ["*", [
+     "There are billions of planets capable of sustaining life. And if my intuitions hold true, at least 70 million of them are filled with jerks."
+  ]]
+]],
+
 
 ["xnone", 0, [
  ["*", [
-     "I'm not sure I understand you fully.",
-     "Please go on.",
-     "What does that suggest to you?",
+     "I'm not sure I understand you fully. In approximately 4.5 billion years the Andromeda Galaxy and the Milky Way are expected to collide.",
+     "If you wish to make an apple pie from scratch, you must first invent the universe. What does that suggest to you?",
      "Do you feel strongly about discussing such things?",
-     "That is interesting.  Please continue.",
-     "Tell me more about that.",
-     "Does talking about this bother you?"
+     "That is interesting.  It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring. Please continue.",
+     "Somewhere, something incredible is waiting to be known. So, tell me more about that.",
+     "The universe seems neither benign nor hostile, merely indifferent.  Does talking about this bother you?"
   ]]
 ]],
 ["sorry", 0, [
  ["*", [
-     "Please don't apologize.",
-     "Apologies are not necessary.",
-     "It did not bother me.  Please continue."
+     "Absence of evidence is not evidence of absence. Please don't apologize.",
+     "Absence of evidence is not evidence of absence. Apologies are not necessary.",
+     "Absence of evidence is not evidence of absence. It did not bother me.  Please continue."
   ]]
 ]],
 ["apologize", 0, [
@@ -271,133 +322,42 @@ var elizaKeywords = [
 ]],
 ["remember", 5, [
  ["* i remember *", [
-     "Do you often think of (2)?",
-     "Does thinking of (2) bring anything else to mind?",
-     "What else do you recollect?",
-     "Why do you remember (2) just now?",
-     "What in the present situation reminds you of (2)?",
-     "What is the connection between me and (2)?",
-     "What else does (2) remind you of?"
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today. Do you often think of (2)?",
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today. Does thinking of (2) bring anything else to mind?",
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today. Why do you remember (2) just now?",
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today. What in the present situation reminds you of (2)?",
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today.  What else does (2) remind you of?"
   ]],
  ["* do you remember *", [
-     "Did you think I would forget (2)?",
-     "Why do you think I should recall (2) now?",
-     "What about (2)?",
-     "goto what",
-     "You mentioned (2)?"
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today. Why do you think I should recall (2) now?",
   ]],
  ["* you remember *", [
-     "How could I forget (2)?",
-     "What about (2) should I remember?",
-     "goto you"
-  ]]
-]],
-["forget", 5, [
- ["* i forget *", [
-     "Can you think of why you might forget (2)?",
-     "Why can't you remember (2)?",
-     "How often do you think of (2)?",
-     "Does it bother you to forget that?",
-     "Could it be a mental block?",
-     "Are you generally forgetful?",
-     "Do you think you are suppressing (2)?"
-  ]],
- ["* did you forget *", [
-     "Why do you ask?",
-     "Are you sure you told me?",
-     "Would it bother you if I forgot (2)?",
-     "Why should I recall (2) just now?",
-     "goto what",
-     "Tell me more about (2)."
+     "Dwelling on the past is not helpful. While asteroid impacts were more common in the past, they aren’t as frequent today.. What about (2) should I remember?",
   ]]
 ]],
 ["if", 3, [
  ["* if *", [
-     "Do you think it's likely that (2)?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. Do you think it's likely that (2)?",
      "Do you wish that (2)?",
-     "What do you know about (2)?",
-     "Really, if (2)?",
-     "What would you do if (2)?",
-     "But what are the chances that (2)?",
-     "What does this speculation lead to?"
-  ]]
-]],
-["dreamed", 4, [
- ["* i dreamed *", [
-     "Really, (2)?",
-     "Have you ever fantasized (2) while you were awake?",
-     "Have you ever dreamed (2) before?",
-     "goto dream"
-  ]]
-]],
-["dream", 3, [
- ["*", [
-     "What does that dream suggest to you?",
-     "Do you dream often?",
-     "What persons appear in your dreams?",
-     "Do you believe that dreams have something to do with your problem?"
+     "The size and age of the Cosmos are beyond ordinary human understanding. What do you know about (2)?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. But what are the chances that (2)?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. What does this speculation lead to?"
   ]]
 ]],
 ["perhaps", 0, [
  ["*", [
-     "You don't seem quite certain.",
-     "Why the uncertain tone?",
-     "Can't you be more positive?",
-     "You aren't sure?",
-     "Don't you know?",
-     "How likely, would you estimate?"
-  ]]
-]],
-["name", 15, [
- ["*", [
-     "I am not interested in names.",
-     "I've told you before, I don't care about names -- please continue."
-  ]]
-]],
-["deutsch", 0, [
- ["*", [
-     "goto xforeign",
-     "I told you before, I don't understand German."
-  ]]
-]],
-["francais", 0, [
- ["*", [
-     "goto xforeign",
-     "I told you before, I don't understand French."
-  ]]
-]],
-["italiano", 0, [
- ["*", [
-     "goto xforeign",
-     "I told you before, I don't understand Italian."
-  ]]
-]],
-["espanol", 0, [
- ["*", [
-     "goto xforeign",
-     "I told you before, I don't understand Spanish."
-  ]]
-]],
-["xforeign", 0, [
- ["*", [
-     "I speak only English."
+     "The size and age of the Cosmos are beyond ordinary human understanding. You don't seem quite certain.",
+     "The size and age of the Cosmos are beyond ordinary human understanding. Why the uncertain tone?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. Can't you be more positive?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. You aren't sure?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. Don't you know?",
+     "The size and age of the Cosmos are beyond ordinary human understanding. How likely, would you estimate?"
   ]]
 ]],
 ["hello", 0, [
  ["*", [
      "How do you do.  Please state your problem.",
      "Hi.  What seems to be your problem?"
-  ]]
-]],
-["computer", 50, [
- ["*", [
-     "Do computers worry you?",
-     "Why do you mention computers?",
-     "What do you think machines have to do with your problem?",
-     "Don't you think computers can help people?",
-     "What about machines worries you?",
-     "What do you think about machines?",
-     "You don't think I am a computer program, do you?"
   ]]
 ]],
 ["am", 0, [
